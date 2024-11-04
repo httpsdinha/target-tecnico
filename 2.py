e92af5244ee99b2241abfe889df296daa26d476c
Amanda Gois <amandabalcacar@gmail.com>
2024-11-04 10:23:43 -0400

    loop para gerar numeros na sequencia de fibonacci

---
+++ 2.py
@@ -0,0 +1,14 @@
+def is_fibonacci(n):
+    if n < 0:
+        return False
+    a, b = 0, 1
+    while b < n:
+        a, b = b, a + b
+    return b == n or n == 0
+
+
+numero = int(input())
+if is_fibonacci(numero):
+    print(f"O número {numero} pertence à sequência de Fibonacci.")
+else:
+    print(f"O número {numero} não pertence à sequência de Fibonacci.")
